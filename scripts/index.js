cad57bac62d3c287915bf5c769371d1990775f02
Gabriel Santo <gvgesanto2@gmail.com>
2022-05-10 15:38:01 -0300

    chore: Set up Github Actions, linters and formatters

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ const hamburguerMenu = document.getElementById('hamburger-menu');
 const hamburgerBtn = document.getElementById('hamburguer-btn');
 const hamburgerCancelBtn = document.getElementById('hamburger-cancel-btn');
 const hamburguerMenuLinks = document.querySelectorAll(
-  '.o-hamburger-menu__link'
+  '.o-hamburger-menu__link',
 );
 
 // Show Hamburguer Menu event handler
@@ -14,7 +14,7 @@ const showHamburgerMenu = () => {
   homeMainContainer.classList.add('u-is-blurred');
 };
 
-// Close Hamburguer Menu event handler 
+// Close Hamburguer Menu event handler
 const closeHamburgerMenu = () => {
   if (homeMainContainer.classList.contains('u-is-blurred')) {
     homeMainContainer.classList.remove('u-is-blurred');
